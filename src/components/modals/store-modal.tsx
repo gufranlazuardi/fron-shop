ff0e6f0a566d252df739156e1c2712fce19884f7
gufranlazuardi <gufranlazuardi@gmail.com>
2024-08-19 11:44:15 +0700

    feat: form

--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -2,9 +2,38 @@
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import CustomModal from "../CustomModal";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const fromSchema = z.object({
+  name: z.string().min(1),
+});
+
+const onSubmit = async (values: z.infer<typeof fromSchema>) => {
+  // todo: buat toko
+  console.log(values);
+};
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof fromSchema>>({
+    resolver: zodResolver(fromSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
 
   return (
     <CustomModal
@@ -13,7 +42,38 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Store Form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="nama toko" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button
+                  variant={"destructive"}
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                <Button className="bg-green-500" type="submit">
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </CustomModal>
   );
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,24 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// Define which routes are public
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  // If the route is not public, protect it
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
-    "/", // Include the home page
   ],
 };
